98e44877b6d23357d8eed7dd301acf568abbfaad
Pedro <pedromagician@gmail.com>
2022-04-17 22:02:21 +0200

    clean up

--- UTL_Cmd.cpp
+++ UTL_Cmd.cpp
@@ -12,7 +12,7 @@ UTL_Cmd::~UTL_Cmd()
 
 void UTL_Cmd::Help()
 {
-	_tprintf(_T("FileRequester 0.3\n"));
+	_tprintf(_T("FileRequester 0.3.1\n"));
 	_tprintf(_T("	FileRequester for command line. Amiga Rulez!\n"));
 	_tprintf(_T("\nUsage:\n"));
 	_tprintf(_T("	FileRequester [options]\n"));
@@ -68,12 +68,6 @@ int UTL_Cmd::ParseCommandLine(int _argc, _TCHAR* _pArgv[], int& _correctParamete
 						_correctParameters++;
 						unknown = false;
 					}
-					break;
-				}
-				else if (mArguments[a].type == _BOOL) {
-					*((bool*)mArguments[a].pVar) = !*((bool*)mArguments[a].pVar);
-					_correctParameters++;
-					unknown = false;
 					break;
 				}
 				else if (mArguments[a].type == _TRUE) {
