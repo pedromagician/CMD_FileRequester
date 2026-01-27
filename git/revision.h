cc3e5238dce37b9cb67c49cf1807da0e7e825076
Pedro <pedromagician@gmail.com>
2026-01-27 13:20:12 +0100

    adding a revision to version.h

---
+++ git/revision.h
@@ -0,0 +1,3 @@
+#define __BUILD_REVISION__ 1
+#define __BUILD_BRANCH_NAME__ _T("main")
+#define __BUILD_BRANCH_HASH__ _T("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
--- version.h
+++ version.h
@@ -1,10 +1,13 @@
+#include "git/revision.h"
+
 #define STRINGIZE2(s) #s
 #define STRINGIZE(s) STRINGIZE2(s)
 
 #define VERSION_MAJOR               1
 #define VERSION_MINOR               8
 #define VERSION_PATCH               0
-#define VERSION_REVISION            0
+#define VERSION_REVISION            __BUILD_REVISION__
+
 
 #define VER_COMPANYNAME_STR         "Pedro"
 #define VER_FILE_DESCRIPTION_STR    "FileRequester for command line. Amiga Rulez!"
